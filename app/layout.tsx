aa6221bbc187ec8c23857ee6da9b790be47ae640
Mustafa Syahmi <mustafa@cpglobalinnovation.com>
2026-01-29 10:54:39 +0800

    feat(layout): enhance body styling for improved layout structure; bump version to 0.5.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.className} min-h-screen justify-between flex flex-col`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
